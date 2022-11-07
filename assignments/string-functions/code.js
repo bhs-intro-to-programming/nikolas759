bdd9b9f93297e4ecace25f7443ecf6c4c0451341
nikolas759 <114030179+nikolas759@users.noreply.github.com>
2022-11-07 15:07:48 -0800

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -8,10 +8,10 @@ const simplePigLatin = (s,n) => {
   return s.substring(n)+s.substring(0,n)+'ay'
 }
 const firstHalf = (s) => {
-  return substring(s.lenght/2)
+  return substring(s.length/2)
 }
 const lastThree = (s) => {
-  return substring(s.lenght-3)
+  return substring(s.length-3)
   }
   const capitalize = (s) => {
     return s[0].toUpperCase 
