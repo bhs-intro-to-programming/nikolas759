660069b9e5d748def7d657be8c1d799c6053bee3
nikolas759 <114030179+nikolas759@users.noreply.github.com>
2022-11-07 14:58:20 -0800

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -7,3 +7,9 @@ const swapFrontAndBack = (s) => {
 const simplePigLatin = (s,n) => {
   return s.substring(n)+s.substring(0,n)+'ay'
 }
+const isAllUpperCase = (true, false) => {
+  ret
+}
+const firstHalf = (half) => {
+  return substring(s.lenght/2)
+}
