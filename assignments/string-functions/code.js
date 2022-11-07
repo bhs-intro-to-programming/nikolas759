95889b4bea1fae5dd5ce0201d26f9cbcc5a63a60
nikolas759 <114030179+nikolas759@users.noreply.github.com>
2022-11-07 15:11:50 -0800

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -22,6 +22,6 @@ const lastThree = (s) => {
     const allButFirst = (s) => {
       return s.substring (1,s.length)
     }
-    const firstHalf = (s) => {
+const firstHalf = (s) => {
   return s.substring(s.length/2)
-    }
+}
