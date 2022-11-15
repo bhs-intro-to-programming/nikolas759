8ce351b5eb441893634f7958c01848adf2baecc9
nikolas759 <114030179+nikolas759@users.noreply.github.com>
2022-11-15 14:49:04 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -7,8 +7,9 @@ const area = (rect) => {
 const higherPaid = (e1, e2) => {
  let { ‘e1’: 0, ‘e2’: 0 };{
    let (i=0; i < salary.length; i++){
-   if (e1[i] > e2[i]){
-     return e1
+    if (e1[i] > e2[i]){
+     return e1;
+    }
    }
  }
 return e2
