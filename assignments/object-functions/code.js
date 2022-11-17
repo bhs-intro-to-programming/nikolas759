2c6e6642db284840c5ec4a92421891147c226578
nikolas759 <114030179+nikolas759@users.noreply.github.com>
2022-11-17 14:34:24 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -4,8 +4,8 @@ const emptyObject = () => {
 const getX = (obj) => {
   return obj.x
 }
-const sumSalaries = (worker1, worker2) => {
+const sumSalaries = (worker1) => {
   for(let i = 0; i < lenght; i++){
-    return worker1.salary + worker2
+    return worker1.salary
   }
 }
