802c84f07530200d7871a35e7659066003b11151
nikolas759 <114030179+nikolas759@users.noreply.github.com>
2022-11-29 15:18:28 -0800

    Updating assessments/2022-11-29-numeric/code.js

--- assessments/2022-11-29-numeric/code.js
+++ assessments/2022-11-29-numeric/code.js
@@ -8,7 +8,7 @@ const extraChocolates = (chocolates, person) => {
   return (chocolates % person)
 }
 const leftOut = (chocolates, person) => {
-  return (chocolates - person) + Math.max(0, -3)
+  return -(chocolates - person)
 }
 const presentsBudget = (friends, presents) => {
   return friends * presents
