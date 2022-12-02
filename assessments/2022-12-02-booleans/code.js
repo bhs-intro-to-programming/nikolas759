7b0916e0c18f07b1d73862b34967561c92343a64
nikolas759 <114030179+nikolas759@users.noreply.github.com>
2022-12-02 15:01:34 -0800

    Updating assessments/2022-12-02-booleans/code.js

--- assessments/2022-12-02-booleans/code.js
+++ assessments/2022-12-02-booleans/code.js
@@ -12,3 +12,6 @@ const isVenomous = (snake)
 const okaySpeed = (speedLimit, carSpeed) => {
   return (speedLimit - 10) > carSpeed && carSpeed < (speedLimit + 10)
 }
+const twiceAsExpensive = (price1, price2) => {
+  return price2 * 2 === price1
+}
