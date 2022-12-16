f58802ad3760df01d832aa590278fdd083b937f9
nikolas759 <114030179+nikolas759@users.noreply.github.com>
2022-12-16 10:56:34 -0800

    Updating assessments/2022-12-02-booleans/code.js

--- assessments/2022-12-02-booleans/code.js
+++ assessments/2022-12-02-booleans/code.js
@@ -1,21 +1,3 @@
-const needHeavyCoat = (goingOut, cold) => {
-  return goingOut && cold 
-}
-needSunscreen = (goBeach, goSki) => {
-  return goBeach || goSki
-}
-const needMittens = (goOut, warm) => {
-  return goOut && !warm
-}
-const okaySpeed = (speedLimit, carSpeed) => {
-  return (speedLimit - 10) > carSpeed && carSpeed < (speedLimit + 10)
-}
-const twiceAsExpensive = (price1, price2) => {
-  return price2 * 2 === price1
-}
-const winningRecord = (wins, loss, ties) => {
-  return (wins + loss + ties) / 2 < wins
-} 
-const isMagicNumber = (x) => {
-  return x === 42 || x=== 17
+const needHeavyCoat = (cold, outside) => {
+  return cold && outside
 }
