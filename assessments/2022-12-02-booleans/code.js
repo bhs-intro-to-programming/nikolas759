9818eca3fdbcde8b14d37bbd14f4977d5885a263
nikolas759 <114030179+nikolas759@users.noreply.github.com>
2022-12-16 10:58:10 -0800

    Updating assessments/2022-12-02-booleans/code.js

--- assessments/2022-12-02-booleans/code.js
+++ assessments/2022-12-02-booleans/code.js
@@ -4,3 +4,6 @@ const needHeavyCoat = (cold, outside) => {
 const needSunscreen = (beach, skiing) => {
   return beach || skiing 
 }
+const needMittens = (goOut, warm) => {
+  return goOut && !warm
+}
