063e0872a299041a2f7dd674411143f7b0fe98fd
nikolas759 <114030179+nikolas759@users.noreply.github.com>
2022-12-16 11:05:25 -0800

    Updating assessments/2022-12-16-fall-semester-final/code.js

--- assessments/2022-12-16-fall-semester-final/code.js
+++ assessments/2022-12-16-fall-semester-final/code.js
@@ -96,3 +96,18 @@ const pair = (a, b) => {
 
 ////////////////////////////////////////////////////////////////////////
 // Write your code here ...
+const totalEggs = (soft, hard) => {
+  return soft + hard
+}
+const chocolatesPerPerson = (cho, people) => {
+  return Math.floor(cho / people)
+}
+const extraChocolates = (cho, people) => {
+  return cho % people
+}
+const presentsBudget = (friends, presents) => {
+  return friends * presents
+}
+const perPresent = (friends, presents) => {
+  return friends / presents
+}
